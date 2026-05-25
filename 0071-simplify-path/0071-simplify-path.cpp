2ea94466bbfdae138b7cc3bcb28312c74441171e
Ayush Chaudhary <ayushc24@iitk.ac.in>
2026-05-25 22:45:15 +0530

    Time: 11 ms (5.22%), Space: 23.8 MB (5.12%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stringstream ss(path);
+        string token;
+        vector<string> st;
+
+        while(getline(ss,token,'/')){
+            if (token=="" || token==".") continue;
+            else if(token ==".."){
+                if(st.size()==0) continue;
+                else 
+                 st.pop_back();
+            }
+            else st.push_back(token);
+        }
+        string ans = "";
+        for(string word:st){
+            ans = ans+'/'+word;
+            
+        }
+        if (ans=="") ans = "/";
+
+        return ans;
+    }
+};
