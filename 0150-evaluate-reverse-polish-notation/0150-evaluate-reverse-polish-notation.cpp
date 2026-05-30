8411c7b7646168824009fea50a6a46e62bf5dd65
Ayush Chaudhary <ayushc24@iitk.ac.in>
2026-05-30 16:13:54 +0530

    Time: 0 ms (100%), Space: 17 MB (65.25%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        int n = tokens.size();
         stack<int> st;
-        for(int i =0 ;i<n;i++){
-            if(!st.empty() && ( tokens[i] == "*" || tokens[i] == "/" || tokens[i] == "+" || tokens[i] == "-") ){
-                int num1 = st.top();
-                st.pop();
+        int n=tokens.size();
+        if(n==0) return 0;
+
+        for(int i=0; i<n;i++){
+            if(!st.empty()&& (tokens[i]=="+" || tokens[i]=="*"|| tokens[i]=="/"|| tokens[i]=="-")){
                 int num2 = st.top();
                 st.pop();
-                int ans = 0;
-                if(tokens[i] == "*"){
-                     ans = num1* num2;
-                }else if(tokens[i] == "/"){
-                     ans = num2/ num1;
-                }else if(tokens[i] == "+"){
-                     ans = num1+ num2;
-                }else  ans = num2- num1;
-
-                st.push(ans);
-
+                int num1 = st.top();
+                st.pop();
+                if(tokens[i]=="+"){
+                    st.push(num1+num2);
+                }else if(tokens[i]=="*"){
+                    st.push(num1*num2);
+                }else if(tokens[i]=="/"){
+                    st.push(num1/num2);
+                }else if(tokens[i]=="-"){
+                    st.push(num1-num2);
+                }
+                
             }
             else st.push(stoi(tokens[i]));
         }
         return st.top();
+
     }
+
 };
