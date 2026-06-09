53cbf992a4203401b20a5930cce3c5e2d7cda8bb
Ayush Chaudhary <ayushc24@iitk.ac.in>
2026-06-09 17:21:54 +0530

    Time: 3 ms (63.18%), Space: 8 MB (21.97%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    int helper(int n, vector<int> &dp){
-        if(n==0|| n==1) return n;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = helper(n-1, dp) + helper(n-2, dp);
+  
+    int fib(int n){
+        if(n<=1) return n;
+        vector<int> dp(3,-1);
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+            dp[2] = dp[0]+dp[1];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
     }
-    int fib(int n) {
-       vector<int> dp(n+1,-1);
-       return helper(n, dp);
-    }
+	return dp[2];
+}
 };
