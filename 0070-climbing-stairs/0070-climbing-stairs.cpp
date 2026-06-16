f1c97ea2d17cb56f64c8a9043e529072f6975c1b
Ayush Chaudhary <ayushc24@iitk.ac.in>
2026-06-16 11:16:35 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int>dp(3,1);
-        dp[2] = 1;
-        dp[1] = 1;
-
-        for(int i=n-2; i>=0; i--){
-            dp[0] = dp[1]+dp[2];
-            dp[2] = dp[1];
-            dp[1] = dp[0];
+        int t[n+1];
+        t[0] = 1;
+        t[1] = 1;
+        for(int i=2; i<=n;i++){
+            t[i] = +t[i-1]+t[i-2];
         }
-        return dp[0];
+        return t[n];
     }
 };
