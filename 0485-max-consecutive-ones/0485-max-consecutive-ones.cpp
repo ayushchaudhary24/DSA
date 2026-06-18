09f8b9df103efbce63623ca756426acdfbc68f9c
Ayush Chaudhary <ayushc24@iitk.ac.in>
2026-06-18 17:45:27 +0530

    Time: 0 ms (100%), Space: 50 MB (92.92%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -4,8 +4,8 @@ public:
         int ans = 0;
         int n = nums.size();
         int count = 0;
-        for(int i=0; i<n;i++){
-            if(nums[i]==1) count++;
+        for(int x: nums){
+            if(x==1) count++;
             else{
                 ans = max(ans,count);
                 count =0;
